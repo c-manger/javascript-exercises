591dbd357afa96adde2e78de5b92764665a20e01
c-manger <54277781+c-manger@users.noreply.github.com>
2026-08-08 15:56:49 -0400

    Completes exercises 12 and 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let number = Number(num);
+    
+    if (number < 0) {
+        return 'OOPS';
+    }
 
+    if (number === 0) {
+        return 0;
+    }
+
+    const arr =[1];
+
+    for (let i = 1; i < number; i++) {
+        if (i === 1) {
+            arr.push(i);
+        };
+
+        if (i > 1) {
+            const entry = arr[i - 1] + arr[i  -2];
+            arr.push(entry);
+        };
+    };
+
+    return arr[arr.length - 1];
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
